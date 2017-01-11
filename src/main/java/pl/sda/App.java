50df77a135003ea76f828c9800c738d3fd890ce7
mgieysztor <michal.gieysztor@gmail.com>
2017-01-11 18:14:09 +0100

    Update App.java

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello everybody! Dance" );
+        System.out.println( "Hello everybody! Dance and jump!" );
         System.out.println("testujemy gita");
     }
 }
